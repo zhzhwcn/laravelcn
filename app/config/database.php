04d5f982079b00ff4d01e0e678fbef896fd933fe
zhzhwcn <zhzhwmail@gmail.com>
2014-07-07 09:31:06 +0700

    修改数据库连接 请自行创建数据库与用户

--- app/config/database.php
+++ app/config/database.php
@@ -46,42 +46,17 @@ return array(
 
 	'connections' => array(
 
-		'sqlite' => array(
-			'driver'   => 'sqlite',
-			'database' => __DIR__.'/../database/production.sqlite',
-			'prefix'   => '',
-		),
-
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'laravelcn',
+			'username'  => 'laravelcn',
+			'password'  => 'kTHgXE@Cjd2$',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
 		),
 
-		'pgsql' => array(
-			'driver'   => 'pgsql',
-			'host'     => 'localhost',
-			'database' => 'forge',
-			'username' => 'forge',
-			'password' => '',
-			'charset'  => 'utf8',
-			'prefix'   => '',
-			'schema'   => 'public',
-		),
-
-		'sqlsrv' => array(
-			'driver'   => 'sqlsrv',
-			'host'     => 'localhost',
-			'database' => 'database',
-			'username' => 'root',
-			'password' => '',
-			'prefix'   => '',
-		),
 
 	),
 
@@ -109,16 +84,16 @@ return array(
 	|
 	*/
 
-	'redis' => array(
+	// 'redis' => array(
 
-		'cluster' => false,
+		// 'cluster' => false,
 
-		'default' => array(
-			'host'     => '127.0.0.1',
-			'port'     => 6379,
-			'database' => 0,
-		),
+		// 'default' => array(
+			// 'host'     => '127.0.0.1',
+			// 'port'     => 6379,
+			// 'database' => 0,
+		// ),
 
-	),
+	// ),
 
 );
