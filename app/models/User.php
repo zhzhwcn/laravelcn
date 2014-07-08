619b655cc92a75dd935b5f8d9eb53256cac60a26
zhzhwcn <zhzhwmail@gmail.com>
2014-07-08 14:19:23 +0700

    添加user model的softdelete

--- app/models/User.php
+++ app/models/User.php
@@ -4,10 +4,11 @@ use Illuminate\Auth\UserTrait;
 use Illuminate\Auth\UserInterface;
 use Illuminate\Auth\Reminders\RemindableTrait;
 use Illuminate\Auth\Reminders\RemindableInterface;
+use Illuminate\Database\Eloquent\SoftDeletingTrait;
 
 class User extends Eloquent implements UserInterface, RemindableInterface {
 
-	use UserTrait, RemindableTrait;
+	use UserTrait, RemindableTrait, SoftDeletingTrait;
 
 	/**
 	 * The database table used by the model.
