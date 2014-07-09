9455495dc55793c0df71606f6596739fdfa59f48
zhzhwcn <zhzhwmail@gmail.com>
2014-07-09 15:52:32 +0700

    增加注册页面错误提示

--- app/views/users/reg_left.php
+++ app/views/users/reg_left.php
@@ -1,24 +1,33 @@
 <form class="form-horizontal" role="form" action="<?php echo url('/reg');?>" method="post">
+	<?php if($errors->count()){?>
+	<div class="alert alert-danger" role="alert">
+		<?php 
+			foreach ($errors->all('<p>:message</p>') as $message){
+				echo $message;
+			}
+		?>
+	</div>
+	<?php }?>
 	<div class="form-group">
-		<label for="inputEmail3" class="col-sm-2 control-label"><?php echo trans('Username');?></label>
+		<label class="col-sm-2 control-label"><?php echo trans('Username');?></label>
 		<div class="col-sm-4">
 			<input name="username" type="text" class="form-control" placeholder="<?php echo trans('Username');?>">
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="inputEmail3" class="col-sm-2 control-label"><?php echo trans('Email');?></label>
+		<label class="col-sm-2 control-label"><?php echo trans('Email');?></label>
 		<div class="col-sm-4">
 			<input name="email" type="email" class="form-control" placeholder="<?php echo trans('Email');?>">
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="inputPassword3" class="col-sm-2 control-label"><?php echo trans('Password');?></label>
+		<label class="col-sm-2 control-label"><?php echo trans('Password');?></label>
 		<div class="col-sm-4">
 			<input name="password" type="password" class="form-control" placeholder="<?php echo trans('Password');?>">
 		</div>
 	</div>
 	<div class="form-group">
-		<label for="inputPassword3" class="col-sm-2 control-label"><?php echo trans('Password confirm');?></label>
+		<label class="col-sm-2 control-label"><?php echo trans('Password confirm');?></label>
 		<div class="col-sm-4">
 			<input name="password_confirmation" type="password" class="form-control" placeholder="<?php echo trans('Password confirm');?>">
 		</div>
