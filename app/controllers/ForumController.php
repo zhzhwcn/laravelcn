676c4ce6e19a5cc3a797618b9466412d19f29002
zhzhwcn <zhzhwmail@gmail.com>
2014-07-10 14:27:36 +0700

    回复功能完成 暂无分页

--- app/controllers/ForumController.php
+++ app/controllers/ForumController.php
@@ -50,16 +50,43 @@ class ForumController extends BaseController {
 	public function getThread($thread_id = 0)
 	{
 		$thread = Thread::findOrFail($thread_id);
+		if($thread->parent_id){
+			App::abort(404);
+		}
+		$poster = User::findOrFail($thread->user_id);
+		$replys = DB::table('threads')
+							->leftjoin('users','threads.user_id','=','users.id')
+							->select('threads.content','threads.created_at','threads.updated_at','users.username','users.email')
+							->where('parent_id','=',$thread->id)
+							->get();
 		$this->layout->title = $thread->title;
 		$this->layout->description = '';
 		$this->layout->keywords = '';
 		$this->layout->left = View::make('forum.thread_left')
+									->with('poster',$poster)
+									->with('replys',$replys)
 									->with('thread',$thread);
 		$this->layout->right = View::make('forum.thread_right');
 	}
 	
 	public function postReply($thread_id = 0)
 	{
-		
+		$rules = array('content'=>'required|min:2');
+		$validator = Validator::make(Input::all(), $rules);
+		if ($validator->fails()){
+			return Redirect::back()->withErrors($validator);
+		} else {
+			$parent = Thread::findOrFail($thread_id);
+			$thread = new Thread;
+			$thread->title = '';
+			$thread->content = \Michelf\Markdown::defaultTransform(e(Input::get('content')));
+			$thread->node_id = $parent->node_id;
+			$thread->user_id = Auth::user()->id;
+			$thread->parent_id = $parent->id;
+			$thread->replys = 0;
+			$thread->save();
+			DB::table('threads')->where('id','=',$parent->id)->increment('replys');
+			return Redirect::to('/thread/'.$parent->id);
+		}
 	}
 }
--- app/views/forum/thread_left.php
+++ app/views/forum/thread_left.php
@@ -3,13 +3,68 @@
 	<div class="panel-body">
 		<?php echo $thread->content;?>
 	</div>
+	<div class="panel-body">
+		<?php
+			echo trans('Create at:');
+			echo $thread->created_at;
+			echo '&nbsp;';
+			echo trans('Last update:');
+			echo $thread->updated_at;
+			echo '&nbsp;';
+			echo trans('Post by:');
+			echo $poster->username;
+		?>
+	</div>
 
-	<!-- List group
-	<ul class="list-group">
-		<li class="list-group-item">Cras justo odio</li>
-		<li class="list-group-item">Dapibus ac facilisis in</li>
-		<li class="list-group-item">Morbi leo risus</li>
-		<li class="list-group-item">Porta ac consectetur ac</li>
-		<li class="list-group-item">Vestibulum at eros</li>
-	</ul> -->
+</div>
+<div class="panel panel-default">
+	<div class="panel-body">
+		<ul class="media-list">
+			<?php foreach($replys as $reply){?>
+			<li class="media">
+				<div class="media-body">
+					<?php echo $reply->content;?>
+				</div>
+				<div class="media-body">
+				<?php
+					echo trans('Create at:');
+					echo $reply->created_at;
+					echo '&nbsp;';
+					echo trans('Last update:');
+					echo $reply->updated_at;
+					echo '&nbsp;';
+					echo trans('Reply by:');
+					echo $reply->username;
+				?>
+				</div>
+			</li>
+			<?php }?>
+		</ul>
+	</div>
+</div>
+<div class="panel panel-default">
+	<div class="panel-body">
+		<form class="form-horizontal" role="form" action="<?php echo url('/reply/'.$thread->id);?>" method="post">
+			<?php if($errors->count()){?>
+			<div class="alert alert-danger" role="alert">
+				<?php 
+					foreach ($errors->all('<p>:message</p>') as $message){
+						echo $message;
+					}
+				?>
+			</div>
+			<?php }?>
+			<div class="form-group">
+				<div class="col-sm-10">
+					<textarea name="content" id="reply_content" rows="10" class="form-control" placeholder="<?php echo trans('Reply');?>"></textarea>
+				</div>
+			</div>
+			<div class="form-group">
+				<div class="col-sm-10">
+					<input type="hidden" name="parent_id" value="<?php echo $thread->id;?>">
+					<button type="submit" class="btn btn-default"><?php echo trans('Post');?></button>
+				</div>
+			</div>
+		</form>
+	</div>
 </div>
